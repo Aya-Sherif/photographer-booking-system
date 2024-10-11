dec5c6a8458a6b66a3e8c936219cbfc286c39576
Aya-Sherif <s-aya.nassef@zewailcity.edu.eg>
2024-10-11 15:48:32 +0300

    Update Portfolio

--- app/Http/Controllers/PortfolioController.php
+++ app/Http/Controllers/PortfolioController.php
@@ -14,10 +14,11 @@ class PortfolioController extends Controller
     {
         // Fetch all categories
         $categories = Category::whereHas('photos')->get();
-        
+
 
         // Fetch all portfolio items, with their related category data
-        $portfolioItems = Portfolio::with('category')->get();
+        $portfolioItems = Portfolio::with('category')->orderBy('created_at', 'desc') // Order by the created_at column
+            ->get();
 
         // Return view with categories and portfolio items
         return view('front.portfolio', compact('categories', 'portfolioItems'));
